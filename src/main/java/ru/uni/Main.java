9a68cc719cc475746669a3d9305724ee2d321d4e
Savchenko <savchenko.ra@yandex.ru>
2024-07-05 12:43:01 +0300

    Done task 1.

--- src/main/java/ru/uni/Main.java
+++ src/main/java/ru/uni/Main.java
@@ -1,7 +1,10 @@
 package ru.uni;
 
+import ru.uni.practice.Parent;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Parent parent = new Parent();
+        parent.print();
     }
 }
