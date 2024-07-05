801b51a24d94a149f6f8ffb3286a345846a54c43
Savchenko <savchenko.ra@yandex.ru>
2024-07-05 14:38:53 +0300

    Done task 3.

--- src/main/java/ru/uni/HeirClass.java
+++ src/main/java/ru/uni/HeirClass.java
@@ -2,6 +2,11 @@ package ru.uni;
 
 public class HeirClass extends Parent {
 
+    @Override
+    protected String print() {
+        return "Reborn world!";
+    }
+
     public void printMessage() {
         String message = print();
         System.out.println(message);
--- src/main/java/ru/uni/Main.java
+++ src/main/java/ru/uni/Main.java
@@ -2,7 +2,8 @@ package ru.uni;
 
 public class Main {
     public static void main(String[] args) {
+
         HeirClass heir = new HeirClass();
-        heir.printMessage();
+        heir.print();
     }
 }
